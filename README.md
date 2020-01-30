0597ec400e464e4cd3dc8f5b1d8ea1c9d235f9eb
Ezequiel Portela <ezequiel.portela@naranja.com>
2020-01-30 13:10:36 -0300

    add zsh installation

--- README.md
+++ README.md
@@ -3,9 +3,9 @@
 To install or update devins, you should run the install script. To do that, you may either download and run the script manually, or use the following cURL or Wget command:
 
 ```
-curl -o- https://raw.githubusercontent.com/ezeportela/devins/v0.1.0/install.sh | bash
+curl -o- https://raw.githubusercontent.com/ezeportela/devins/v0.1.1/install.sh | bash
 ```
 
 ```
-wget -qO- https://raw.githubusercontent.com/ezeportela/devins/v0.1.0/install.sh | bash
+wget -qO- https://raw.githubusercontent.com/ezeportela/devins/v0.1.1/install.sh | bash
 ```
--- install.sh
+++ install.sh
@@ -7,6 +7,14 @@ echo 'installing curl...'
 sudo apt install curl
 echo 'curl has been installed successfully.'
 
+echo 'installing git...'
+sudo apt install git
+echo 'git has been installed successfully.'
+
+echo 'installing zsh...'
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+echo 'zsh has been installed successfully.'
+
 echo 'installing nvm...'
 wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.2/install.sh | bash
 
@@ -17,9 +25,16 @@ wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.2/install.sh | bash
 nvm install node
 echo 'nvm has been installed successfully.'
 
-echo 'installing git...'
-sudo apt install git
-echo 'git has been installed successfully.'
+echo 'installing vscode...'
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+sudo install -o root -g root -m 644 packages.microsoft.gpg /usr/share/keyrings/
+sudo sh -c 'echo "deb [arch=amd64 signed-by=/usr/share/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+rm packages.microsoft.gpg
+
+sudo apt-get install apt-transport-https
+sudo apt-get update
+sudo apt-get install code
+echo 'vscode has been installed successfully.'
 
 echo 'installing postman...'
 wget https://dl.pstmn.io/download/latest/linux64 -O postman.tar.gz
