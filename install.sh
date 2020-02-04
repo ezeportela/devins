54dee30e0fd55f660c73893e3d7b668f3d1e513d
Ezequiel Portela <ezequiel.portela@naranja.com>
2020-02-04 12:32:01 -0300

    Fix change sh error

--- install.sh
+++ install.sh
@@ -53,5 +53,7 @@ sudo ln -sfn /opt/Postman/Postman /usr/bin/postman
 # ============================================
 sudo apt install zsh -y
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-chsh -s ${which zsh}
+. ~/.profile
+. ~/.bashrc
+chsh -s $(which zsh)
 # ============================================
