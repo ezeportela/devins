77dd7781b6d1177a04ef022649f84997072decdb
ezeportela <eportelab@gmail.com>
2020-09-16 16:10:10 -0300

    Update python.sh

--- python.sh
+++ python.sh
@@ -1,6 +1,9 @@
+#!/usr/bin/env bash
+
 sudo apt install -y python3-pip
 sudo apt install -y python3-venv
 echo 'alias py=python3' >> ~/.zshrc
 echo 'alias install_pyenv="python3 -m venv venv"' >> ~/.zshrc
 echo 'alias pyenv="source venv/bin/activate"' >> ~/.zshrc
 echo 'alias upyenv="deactivate"' >> ~/.zshrc
+echo 'alias pyenv_install="pip3 install -r requirements.txt"' >> ~/.zshrc
