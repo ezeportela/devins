721418832e6d29a34c85a4563193384ba94b93d9
ezeportela <eportelab@gmail.com>
2021-02-26 10:15:03 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@ sudo apt install curl git alacarte fonts-firacode fonts-powerline -y
 
 # node
 # ============================================
-wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.2/install.sh | bash
+wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | bash
 
 . ~/.nvm/nvm.sh
 . ~/.profile
