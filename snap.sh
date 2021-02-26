3a5784a21205e23a3a6564103c8484c1f42f7cff
ezeportela <eportelab@gmail.com>
2021-02-26 10:15:39 -0300

    Update snap.sh

--- snap.sh
+++ snap.sh
@@ -2,6 +2,5 @@ sudo snap install redis-desktop-manager
 sudo snap install robo3t-snap
 sudo snap install postman
 sudo snap install sublime-text --classic
-sudo snap install sublime-merge --classic
 sudo snap install vlc
 sudo snap install slack --classic
